c5d2d11a1438b281294bddd75ea1d496ed274c19
Caleb Brazeau <cjbrazeau12@gmail.com>
2023-03-07 23:40:47 -0500

    Import and Implement A* path finding for enemy

--- Cita 412 Projects/Assets/Scripts/Enemies/Enemy.cs
+++ Cita 412 Projects/Assets/Scripts/Enemies/Enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    GameObject target;
+    AstarAI AIScript;
 
     [SerializeField] float speed;
     [SerializeField] float targetResetTimer;
@@ -14,11 +14,7 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
-    }
-
-    void Update()
-    {
-        FollowTarget();
+        AIScript = GetComponent<AstarAI>();
     }
 
     void OnTriggerStay(Collider collider) {
@@ -27,30 +23,20 @@ public class Enemy : MonoBehaviour
             // Stop the coroutine that removes the target object
             StopCoroutine(ResetTarget());
 
+            AIScript.enabled = true;
             // If there is not already a target object set the target to the collided object
-            if (!target) SetTarget(collider.gameObject);
+            if (!AIScript.targetPosition) SetTarget(collider.transform);
         }
     }
 
     void OnTriggerExit(Collider collider) {
         // If the exited object is the target object start timer to reset target variable
-        if (collider.gameObject.tag == target.tag) StartCoroutine(ResetTarget());
+        if (collider.gameObject.tag == AIScript.targetPosition.tag) StartCoroutine(ResetTarget());
     }
 
-    void FollowTarget() {
-        // Return if there is no target
-        if (!target) return;
-
-        // Step speed of the enemy moving towards the target
-        float step = speed * Time.deltaTime;
-
-        // Move the object towards the target position
-        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
-    }
-
-    // Sets the target Game Object
-    public void SetTarget(GameObject target) {
-        this.target = target;
+    // Sets the target transform
+    public void SetTarget(Transform target) {
+        AIScript.targetPosition = target;
     }
 
     // Method to "forget" the target object by setting it to null after a set amount of time
@@ -60,5 +46,6 @@ public class Enemy : MonoBehaviour
         
         // Set target object to null to stop following
         SetTarget(null);
+        AIScript.enabled = false;
     }
 }
