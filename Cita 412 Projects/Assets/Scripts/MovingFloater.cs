0fb2fb78124fd0fc81151e52151a346571f8774f
Chino Beach <beach988@morrisville.edu>
2023-03-08 20:56:22 -0500

    Started script for moving platforms

---
+++ Cita 412 Projects/Assets/Scripts/MovingFloater.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingFloater : MonoBehaviour
+{
+    [SerializeField] float fltXspeed;
+    [SerializeField] float fltYspeed;
+    [SerializeField] float fltZspeed;
+
+    [SerializeField] float fltMaxX;
+    [SerializeField] float fltMinX;
+    [SerializeField] float fltMaxY;
+    [SerializeField] float fltMinY;
+    [SerializeField] float fltMaxZ;
+    [SerializeField] float fltMinZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //moves the floater left and right
+    void MoveLeftRight()
+    {
+        if(this.GetComponentInParent<>)
+    }
+    void MoveUpDown()
+    {
+
+    }
+
+    void MoveInOut()
+    {
+
+    }
+
+}
