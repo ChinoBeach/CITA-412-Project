3631dafa9a7277c656cd4bf03c8ebe801435da56
Chino Beach <beach988@morrisville.edu>
2023-03-10 11:33:51 -0500

    work done

--- Cita 412 Projects/Assets/Scripts/MovingFloater.cs
+++ Cita 412 Projects/Assets/Scripts/MovingFloater.cs
@@ -4,29 +4,51 @@ using UnityEngine;
 
 public class MovingFloater : MonoBehaviour
 {
+    //how much the object will be incremeted in each axis
     [SerializeField] float fltXspeed;
     [SerializeField] float fltYspeed;
     [SerializeField] float fltZspeed;
 
+    //where the object starts and ends at
+    Vector3 vect3StartingPos;
+    [SerializeField] Vector3 vect3EndingPos;
+
+    
+    /* Might come back to this. Was considering using it for turn around points.
     [SerializeField] float fltMaxX;
     [SerializeField] float fltMinX;
     [SerializeField] float fltMaxY;
     [SerializeField] float fltMinY;
     [SerializeField] float fltMaxZ;
     [SerializeField] float fltMinZ;
+    */
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //get the starting postion 
+        vect3StartingPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(vect3StartingPos.x != vect3EndingPos.x)
+        {
+            MoveLeftRight();
+        }
+        if (vect3StartingPos.y != vect3EndingPos.y)
+        {
+            MoveUpDown();
+        }
+        if (vect3StartingPos.z != vect3EndingPos.z)
+        {
+            MoveInOut();
+        }
+
+
     }
-    //moves the floater left and right
+    //Called when the staring pos x is not equal to the ending 
     void MoveLeftRight()
     {
        
