4b99755d16f4bad64113b5fd256ac3880ff5a97f
Chino Beach <beach988@morrisville.edu>
2023-04-12 13:38:17 -0400

    Update PlayerAttacks.cs

--- Cita 412 Projects/Assets/Scripts/Player/PlayerAttacks.cs
+++ Cita 412 Projects/Assets/Scripts/Player/PlayerAttacks.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class PlayerAttacks : MonoBehaviour
 {
+    /*
+     * Thoughtspace: 
+     * need to do a targeting system that pulls the closest enemy using tags. 
+     * I added tags to the enemies
+     * need to deal damage to the enemy
+     * need to check in range raycast length and distance to see if hit
+     * check to see if it still hits 
+     * need to wait to do damage until it checks if it hits
+     * check in range check if hit
+     * 
+     * projectile caster and and projectile script
+     */
+
+    //Variables
+
+   // [SerializeField] target
+
     // Start is called before the first frame update
     void Start()
     {
